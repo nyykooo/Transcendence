f25b10ec8e34f08fa3805b476421c9c57d071d26
nyykooo <nicholas.cakla@outlook.com>
2026-03-02 19:55:02 +0000

    TND_FE: removing Page component

--- tnd/src/App.tsx
+++ tnd/src/App.tsx
@@ -1,6 +1,5 @@
 // import { BrowserRouter } from 'react-router-dom'
 import { Box, Stack } from '@mui/material';
-import Page from './components/Page';
 import Header from './components/Header';
 import Router from './routes/Router';
 
@@ -9,7 +8,6 @@ export default function App() {
             <Box sx={{display: 'flex', flexDirection: 'column', height: '100vh', width: '100%'}}>
                 <Header/>
                 <Router />
-                <Page route='home' label="home"/>
             </Box>
     );
 }
