eb441c6af7a07d3d753edb1aa54a34ef0195f761
nyykooo <nicholas.cakla@outlook.com>
2026-03-05 12:32:42 +0000

    TND_FE: fixing dev tools error on Footer component, starting to use BottomNavigationAction component

--- tnd/src/components/Footer.tsx
+++ tnd/src/components/Footer.tsx
@@ -1,19 +1,10 @@
-import  { BottomNavigation, Typography } from "@mui/material";
+import  { BottomNavigation, BottomNavigationAction,  Typography } from "@mui/material";
 
 export default function Footer() {
     return (
-        <BottomNavigation
-            sx={{
-                color: "black",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "60px",
-            }}
-        >
-            <Typography variant="body2" color="textSecondary">
-                &copy; 2026 Transcendence. All rights reserved.
-            </Typography>
-        </BottomNavigation>
+    <BottomNavigation showLabels>
+        <BottomNavigationAction label="Home" />
+        <BottomNavigationAction label=" &copy; 2026 Transcendence. All rights reserved." />
+    </BottomNavigation>
     );
 }
