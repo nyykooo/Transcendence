2db07133d76c561e36fdcdc1e38ef0cec3c3494b
nyykooo <nicholas.cakla@outlook.com>
2026-03-17 23:25:51 +0000

    BIO_FE: updating RecipeListView component
    adding datagrid, and tabletoolbar with multipleSelects and SliderSelector

---
+++ frontend/src/views/recipe-list/recipe-list-table-toolbar.tsx
@@ -0,0 +1,116 @@
+import { useState } from 'react';
+
+import { Box, Stack, type SelectChangeEvent } from '@mui/material';
+
+import { MultipleSelect, SliderSelector } from '../../components/components';
+
+export default function RecipeListTableToolbar() {
+
+    // ### Diets ###
+    // mock data for testing, replace with actual data fetching logic
+    const diets = [
+        'Vegetarian',
+        'Vegan',
+        'Keto',
+        'Paleo',
+        'Gluten-Free',
+        'Low-Carb'
+    ];
+
+    const [selectedDiets, setSelectedDiets] = useState<string[]>([]);
+    
+    const handleChangeDiet = (event: SelectChangeEvent<typeof diets>) => {
+        const {
+            target: { value },
+        } = event;
+        setSelectedDiets(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split('.') : value,
+        );
+    };
+
+
+    // ### Ingredients ###
+    // mock data for testing, replace with actual data fetching logic
+    const ingredients = [
+        'Ingredient 1',
+        'Ingredient 2',
+    ];
+
+    const [selectedIngredients, setSelectedIngredients] = useState<string[]>([]);
+
+    const handleChangeIngredients = (event: SelectChangeEvent<typeof ingredients>) => {
+        const {
+            target: { value },
+        } = event;
+        setSelectedIngredients(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+
+
+    // ### Cost ###
+    const [cost, setCost] = useState<number[]>([0, 10.0]);
+
+    const handleChangeCost = (event: Event, newValue: number[], activeThumb: number) => {
+        if (activeThumb === 0) {
+        setCost([Math.min(newValue[0], cost[1] - 1), cost[1]]);
+        } else {
+        setCost([cost[0], Math.max(newValue[1], cost[0] + 1)]);
+        }
+    };
+
+    
+    // ### Portion ###
+    const [portion, setPortion] = useState<number[]>([0, 10]);
+
+    const handleChangePortion = (event: Event, newValue: number[], activeThumb: number) => {
+        if (activeThumb === 0) {
+        setPortion([Math.min(newValue[0], portion[1] - 1), portion[1]]);
+        } else {
+        setPortion([portion[0], Math.max(newValue[1], portion[0] + 1)]);
+        }
+    };
+
+    return (
+        <Box sx={{width: '100%', height: '20%'}}>
+            <Stack direction="row" spacing={2}>
+                {/* Diet == Multiple Select */}
+                <MultipleSelect 
+                    name="Diets"
+                    options={diets}
+                    selectedOptions={selectedDiets}
+                    onChange={handleChangeDiet}
+                />
+                {/* Ingredients == Multiple Select */}
+                <MultipleSelect 
+                    name="Ingredients"
+                    options={ingredients}
+                    selectedOptions={selectedIngredients}
+                    onChange={handleChangeIngredients}
+                />
+                <Stack direction="column" spacing={1} sx={{width: '30%'}}>
+                    {/* Cost == Slider Selector */}
+                    <SliderSelector
+                        value={cost}
+                        valueText="€"
+                        onChange={handleChangeCost}
+                        min={0}
+                        max={10}
+                        name='Cost'
+                    />
+                    {/* Portion == Slider Selector */}
+                    <SliderSelector
+                        value={portion}
+                        valueText="units"
+                        onChange={handleChangePortion}
+                        min={0}
+                        max={10}
+                        name='Portion'
+                    />
+                </Stack>
+            </Stack>
+        </Box>
+    );
+}
--- frontend/src/views/recipe-list/recipe-list-view.tsx
+++ frontend/src/views/recipe-list/recipe-list-view.tsx
@@ -1,15 +1,75 @@
-import { useState } from 'react';
+import { Box } from '@mui/material';
 
-import { Box, Typography } from '@mui/material';
+import { DataGrid, type GridColDef } from '@mui/x-data-grid';
+
+import RecipeListTableToolbar from './recipe-list-table-toolbar';
 
 export default function RecipeListView() {
-    const [title, setTitle] = useState('Recipes List');
-    const [content, setContent] = useState("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum");
+
+    // mock data for testing, replace with actual data fetching logic
+    const rows = [
+    { id: 1, name: 'Recipe 1', ingridients: 'Ingredient 1, Ingredient 2, Ingredient 3', diet: 'Vegetarian', cost: 10.99, portions: 4 },
+    { id: 2, name: 'Recipe 2', ingridients: 'Ingredient 3, Ingredient 4', diet: 'Vegan', cost: 15.50, portions: 6 },
+    { id: 3, name: 'Recipe 3', ingridients: 'Ingredient 5, Ingredient 6', diet: 'Keto', cost: 12.75, portions: 5 },
+    { id: 4, name: 'Recipe 4', ingridients: 'Ingredient 7, Ingredient 8', diet: 'Paleo', cost: 18.25, portions: 8 },
+    { id: 5, name: 'Recipe 5', ingridients: 'Ingredient 9, Ingredient 10', diet: 'Gluten-Free', cost: 20.00, portions: 10 },
+    { id: 6, name: 'Recipe 6', ingridients: 'Ingredient 11, Ingredient 12', diet: 'Low-Carb', cost: 25.50, portions: 12 },
+    ];
+
+    // updateFilters
+
+    const columns: GridColDef<(typeof rows)[number]>[] = [
+    {
+        field: 'name',
+        headerName: 'Name',
+        flex: 1,
+        editable: true,
+    },
+    {
+        field: 'ingridients',
+        headerName: 'Ingredients',
+        flex: 2,
+        editable: true,
+    },
+    {
+        field: 'diet',
+        headerName: 'Diet',
+        flex: 1,
+    },
+    {
+        field: 'cost',
+        headerName: 'Cost',
+        type: 'number',
+        flex: 1,
+        editable: true,
+    },
+    {
+        field: 'portions',
+        headerName: 'Portions',
+        type: 'number',
+        flex: 1,
+        editable: true,
+    },
+    ];
 
     return (
         <Box sx={{height: '100%', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2}}>
-            <Typography variant="h2">{title}</Typography>
-            <Typography align='center' variant="body1">{content}</Typography>
+            <RecipeListTableToolbar/> {/*passar a updateFilters como prop (callback)*/}
+            <Box sx={{ height: 400, width: '100%' }}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    initialState={{
+                    pagination: {
+                        paginationModel: {
+                        pageSize: 5,
+                        },
+                    },
+                    }}
+                    pageSizeOptions={[5]}
+                    disableRowSelectionOnClick
+                />
+            </Box>
         </Box>
     );
 }
