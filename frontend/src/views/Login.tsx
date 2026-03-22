e6a4d9a49deb9ef6986fecce43e020a0e7b1d7df
nyykooo <nicholas.cakla@outlook.com>
2026-03-22 16:05:02 +0000

    BIO_FE: login
    updating login, now useState for user and pass, and handleSubmitLogin function

--- frontend/src/views/Login.tsx
+++ frontend/src/views/Login.tsx
@@ -1,4 +1,6 @@
-import { Box, Stack, TextField } from '@mui/material'
+import { useState } from 'react';
+
+import { Box, Stack, TextField, Button } from '@mui/material'
 
 import { Logo } from '../components/components'
 
@@ -6,16 +8,47 @@ import { images } from '../configs/images'
 
 export default function Login()
 {
+    const [pass, setPass] = useState<string>('');
+
+    const handleUpdatePass = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPass(event.target.value);
+    };
+
+    const  [user, setUser] = useState<string>('');
+
+    const handleUpdateUser = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUser(event.target.value);
+    };
+
+    const handleSubmitLogin = () => {
+        const sendData = {
+            user: user,
+            pass: pass
+        }
+
+        console.log(sendData);
+    }
+
     return (
         <Box sx={{height: '100%', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2}}>
             <Logo size={300} path={images.icons.logo}/>
             <Stack sx={{border: "black", borderRadius: "12px", display: "flex", flexDirection: "column"}}>
-                <TextField label="user"/>
+                <TextField 
+                    label="user"
+                    onChange={handleUpdateUser}
+                />
                 <TextField 
                     id="outlined-password-input"
                     label="Password"
                     type="password"
-                    autoComplete="current-password"/>
+                    autoComplete="current-password"
+                    onChange={handleUpdatePass}
+                />
+                <Button
+                    onClick={handleSubmitLogin}
+                >
+                    Submit
+                </Button>
             </Stack>
         </Box>
     );
