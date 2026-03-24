b5271d7c6ee4c743187362237282dd4aed31ee86
nyykooo <nicholas.cakla@outlook.com>
2026-03-24 18:41:26 +0000

    BIO_FE: User login
    Login functionality implemented alongside the authProvider and protectedroutes components to protect against unauthenticated and unlogged-in users

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import { type ProtectedRouteProps } from "../props/protectedRouteprops";
+import { useAuth } from "./AuthProvider";
+import { useEffect } from "react";
+
+export default function ProtectedRoute({ children }: ProtectedRouteProps) {
+    const { user } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+    if (user === null)
+        navigate('/Login', { replace: true });
+    }, [navigate, user]);
+
+    return children;
+}
---
+++ frontend/src/props/loginProps.tsx
@@ -0,0 +1,24 @@
+export type LoginProps = {
+    username: string;
+    password: string;
+};
+
+export type LoginResponse = {
+    message: string;
+    user: {
+        id: number;
+        email: string;
+        password: string;
+    };
+    token: string;
+}
+
+export type RegisterResponse = {
+    message: string;
+    user: {
+        id: number;
+        email: string;
+        password: string;
+    };
+    token: string;
+}
