7048fd3a287d5247fb55f6228736393831d96d0f
nyykooo <nicholas.cakla@outlook.com>
2026-03-28 17:42:43 +0000

    BIO_FE
    trying to optimize dome gallery component

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -4,7 +4,7 @@ import { Box } from '@mui/material';
 export default function Home() {
     return (
         <Box sx={{height: '100%', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2}}>
-            <DomeGallery minRadius={1600} fit={1} grayscale={false} />
+            <DomeGallery minRadius={1600} fit={1} grayscale={false} segments={20} />
         </Box>
     );
 }
