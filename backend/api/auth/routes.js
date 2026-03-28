8036abd135393ab88bf6c092fc6ddc7037906796
f-amador <framador99@gmail.com>
2026-03-28 14:38:23 +0000

    BIO_BE:
    Finishing image upload

--- backend/api/auth/routes.js
+++ backend/api/auth/routes.js
@@ -40,8 +40,12 @@ router.post('/register', async (req, res) => {
 
     const created = await pool.query(
         `INSERT INTO dev_dba.users (password, email, is_active, last_login)
-        VALUES ($1, false, NOW())
+        VALUES ($1, $2, false, NOW())
         RETURNING  name, is_active, created_at, last_login`,
+=======
+        VALUES ($1, $2, , NOW())
+        RETURNING  nick, is_active, created_at, last_login`,
+>>>>>>> Stashed changes
         [ passwordHash, normalizedEmail]
       );
       const newuser = { ...created.rows[0], avatar: null };
@@ -110,41 +114,15 @@ router.get(['/profile' ,'/auth'], requireAuth, (req, res) => {
 router.post('/profile/avatar', requireAuth, upload.single('avatar'), async (req, res) => {
     // This route will:
     const file = req.file;
-    
+    // 1. Check if a file was uploaded
     if (!file)
       return res.status(400).json({error: 'No file uploaded'});
-    
-    const userId = req.user?.id;
-    if (!userId)
-      return res.status(401).json({error: 'Unauthorized'});
-    
-    const avatarUrl = `${req.protocol}://${req.get('host')}/uploads/avatars/${file.filename}`;
-
-    try {
-      const updated = await pool.query(
-        `UPDATE dev_dba.users
-        SET avatar = $1
-        WHERE id = $2
-        RETURNING id, email, name, avatar, is_active`,
-       [avatarUrl, userId]
-      );
-      if (updated.rowCount === 0)
-           return res.status(404).json({error: "User does not exist"});
-      return res.status(200).json({
-        message: 'Avatar updated',
-        user: updated.rows[0],
-        avatar: avatarUrl,
-      });
-    }
-
-    catch (error)
-    {
-      return res.status(500).json({
-        error: 'Failed to update avatar',
-        details: error.message,
-    });
-  }
-});
+    // 3. Save the avatar URL to the user object
+  const avatarUrl = `${req.protocol}://${req.get('host')}/uploads/avatars/${file.filename}`;
+  // 4. Return the avatar URL
+  return res.status(200).json({ avatar: avatarUrl });
+})
+;
 
 
 // Simple (training-only) global state store.
