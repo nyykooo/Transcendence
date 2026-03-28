63cc071c021c89b78fcb04cafb171b104a515e7a
nyykooo <nicholas.cakla@outlook.com>
2026-03-28 14:43:01 +0000

    BIO_FE
    Optimizing frontend docker build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 // import { BrowserRouter } from 'react-router-dom'
-import { Box, Stack } from '@mui/material';
+import { Box } from '@mui/material';
 import { Header, Footer } from './components/components';
 import Router from './routes/Router';
 
