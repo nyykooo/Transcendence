e807c1918bf17a6b45d92b94c7c19ec938cd4037
nyykooo <nicholas.cakla@outlook.com>
2026-04-08 14:36:44 +0100

    BIO_FE
    Token auth in every navigation

--- frontend/src/configs/api.tsx
+++ frontend/src/configs/api.tsx
@@ -1,6 +1,7 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '/api';
 
 export const api = {
+    checkToken: `${API_BASE_URL}/auth`,
     login: `${API_BASE_URL}/login`,
     githubAuth: `${API_BASE_URL}/auth/github`,
     register: `${API_BASE_URL}/register`,
