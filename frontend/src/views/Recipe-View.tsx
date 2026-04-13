67537184ac0b4d6ee1f920466476c1dd5c46fbfb
nyykooo <nicholas.cakla@outlook.com>
2026-04-13 18:51:18 +0100

    BIO: Recipes
    Updating frontend to show image, ingridients and preparation method in Recipe page

--- frontend/src/views/Recipe-View.tsx
+++ frontend/src/views/Recipe-View.tsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 
-import { Box, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
+
+import { Logo } from '../components/components';
 
 import { useParams } from 'react-router-dom';
 
@@ -13,20 +15,28 @@ export default function RecipeView() {
 
     const [recipe, setRecipe] = useState<Recipe | null>(null);
 
-    const [title, setTitle] = useState(recipe?.name || '');
-
     useEffect(() => {
         const fetchRecipe = async () => {
             const res = await getRecipe(name || '');
             setRecipe(res);
-            setTitle(res?.name || 'Empty Recipe name!');
         };
         fetchRecipe();
     }, [name]);
 
     return (
         <Box sx={{height: '100%', width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2}}>
-            <Typography variant="h2">{title}</Typography>
+            <Typography variant="h2">{recipe?.name || 'Empty Recipe name'}</Typography>
+            <Stack sx={{display: 'flex', flexDirection: 'row', gap: 4, alignItems: 'center'}}>
+                <Logo size={300} path={recipe?.image || ''}/>
+                <Box sx={{display: 'flex', flexDirection: 'column', gap: 2}}>
+                    <Typography variant="h4">Ingridients:</Typography>
+                    {recipe?.ingridients?.map((ingridient, index) => (
+                        <Typography key={index}>{`${ingridient.quantity} ${ingridient.unit} of ${ingridient.name}`}</Typography>
+                    ))}
+                    <Typography variant="h4">Instructions:</Typography>
+                    <Typography>{recipe?.instructions}</Typography>
+                </Box>
+            </Stack>
         </Box>
     );
 }
