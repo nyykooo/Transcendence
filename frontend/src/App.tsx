1eacaf8586185b99a4901e5918304d6d32e5ced4
Nicholas Campbell Caklamanis <129904142+nyykooo@users.noreply.github.com>
2026-05-14 10:45:23 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,18 @@
-import { Box, ThemeProvider } from '@mui/material';
+import { Box, ThemeProvider, CssBaseline } from '@mui/material';
 import Router from './routes/Router';
-import { primaryBackgroundGradient } from './configs/theme';
 import { theme } from './components/components';
 
 export default function App() {
     return (
         <ThemeProvider theme={theme}>
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    minHeight: '100vh',
-                    width: '100%',
-                    overflowX: 'hidden',
-                    background: primaryBackgroundGradient,
-                }}
-            >
+            <CssBaseline />
+            <Box sx={{ 
+                display: 'flex',
+                flexDirection: 'column',
+                minHeight: '100vh',
+                width: '100%',
+                overflowX: 'hidden'
+            }}>
                 <Router />
             </Box>
         </ThemeProvider>
