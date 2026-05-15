8e4d20c1a4fc7f9f83618d2247b4875f8bab3f50
f-amador <framador99@gmail.com>
2026-05-15 17:07:12 +0100

    rebasing

--- frontend/src/views/Recipe-View.tsx
+++ frontend/src/views/Recipe-View.tsx
@@ -107,6 +107,7 @@ export default function RecipeView() {
         } finally {
             setLiking(false);
         }
+    };
     const handleExportToCSV = () => {
         if (!recipe) return;
 
